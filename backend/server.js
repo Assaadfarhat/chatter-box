0595d09609de4e6e79847e58081f0159fd5f4911
Assaad Farhat <spidysensethe1@gmail.com>
2026-04-17 09:37:27 +0300

    Add error logging

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,12 @@
+process.on('uncaughtException', (err) => {
+  console.error('UNCAUGHT EXCEPTION:', err.message, err.stack);
+  process.exit(1);
+});
+process.on('unhandledRejection', (err) => {
+  console.error('UNHANDLED REJECTION:', err);
+  process.exit(1);
+});
+
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
